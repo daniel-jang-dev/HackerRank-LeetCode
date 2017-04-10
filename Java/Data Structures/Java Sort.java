d79c2c8589ca6a4a39005b9ed9101eeeeb8eb450
Yunha Jang <daniel.jang@programmer.net>
2017-04-10 01:12:24 -0400

    Create Java Sort.java

---
+++ Java/Data Structures/Java Sort.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;  
+class Student{
+   private int id;
+   private String fname;
+   private double cgpa;
+   public Student(int id, String fname, double cgpa) {
+      super();
+      this.id = id;
+      this.fname = fname;
+      this.cgpa = cgpa;
+   }
+   public int getId() {
+      return id;
+   }
+   public String getFname() {
+      return fname;
+   }
+   public double getCgpa() {
+      return cgpa;
+   }
+}
+//Complete the code
+public class Solution
+{
+   public static void main(String[] args){
+      Scanner in = new Scanner(System.in);
+      int testCases = Integer.parseInt(in.nextLine());
+      
+      List<Student> studentList = new ArrayList<Student>();
+      while(testCases>0){
+        int id = in.nextInt();
+        String fname = in.next();
+        double cgpa = in.nextDouble();
+
+        Student st = new Student(id, fname, cgpa);
+        studentList.add(st);
+
+        testCases--;
+      }
+      Comparator<Student> byCGPA = new Comparator<Student>() {
+        @Override
+        public int compare(Student s1, Student s2) {
+          if(s1.getCgpa() > s2.getCgpa())
+            return -1;
+          if(s1.getCgpa() < s2.getCgpa())
+            return 1;
+          return 0;
+        };
+      };
+     
+      Comparator<Student> byName = new Comparator<Student>() {
+        @Override
+        public int compare(Student s1, Student s2) {
+          return s1.getFname().compareTo(s2.getFname());
+        };
+      };
+        Collections.sort(studentList, byName); 
+        Collections.sort(studentList, byCGPA);      
+      
+      for(Student st: studentList){
+        System.out.println(st.getFname());
+      }
+   }
+}
+
+
+
